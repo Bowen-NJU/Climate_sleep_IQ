eb6b7d3f2d01644e0341c75278f1f5e812e2e2a1
Bowen-NJU <89908664+Bowen-NJU@users.noreply.github.com>
2026-01-02 16:14:11 +1100

    Update eco_loss.r

--- Code/eco_loss.r
+++ Code/eco_loss.r
@@ -52,6 +52,7 @@ p2 <- ggplot(lost_, aes(x = GDP_per_capita ,y = avetotal,fill = ssp)) +
   labs(x="GDP per capita", y = "Relative economical loss (%)")+
   geom_errorbar(aes(ymin=lowtotal, ymax=hightotal),position=position_dodge(.9), width=.2,size=0.8)+
   scale_fill_manual(values = c("#FF1493", "#CCEDB1", "#41B7C4"))+
-  scale_y_continuous(expand=c(0,0),limits =  c(0,.1),breaks = c(0,0.025,0.050,0.075,0.1),labels = c("0","0.25%","0.5%","0.75%","1.0%"))+
+  scale_y_continuous(expand=c(0,0),limits =  c(0,1),breaks = c(0,0.25,0.50,0.75,1),labels = c("0","0.25%","0.5%","0.75%","1.0%"))+
   scale_x_continuous(limits =  c(0.5,10.5),breaks = seq(from=1, to=10, by=1))
+
 
