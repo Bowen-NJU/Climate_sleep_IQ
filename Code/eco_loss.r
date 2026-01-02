1039840ef103b6a7b89448b7e54bd78a931f2663
Bowen-NJU <89908664+Bowen-NJU@users.noreply.github.com>
2026-01-02 16:12:55 +1100

    Update eco_loss.r

--- Code/eco_loss.r
+++ Code/eco_loss.r
@@ -39,7 +39,7 @@ p1 <- ggplot(lost_, aes(x = reorder(region,r) ,y = avetotal,fill = ssp)) +
   scale_y_continuous(expand=c(0,0),limits =  c(0,1.1),breaks = c(0,0.25,0.50,0.75,1.00),labels = c("0","0.25%","0.5%","0.75%","1.0%"))
 
 
-p2 <- ggplot(lost_, aes(x = GDP_per_capita5 ,y = avetotal,fill = ssp)) +
+p2 <- ggplot(lost_, aes(x = GDP_per_capita ,y = avetotal,fill = ssp)) +
   theme_bw()  +
   geom_bar(position=position_dodge(),stat = "identity")+
   theme(axis.text = element_text(size = 12,face = "bold"),
@@ -54,3 +54,4 @@ p2 <- ggplot(lost_, aes(x = GDP_per_capita5 ,y = avetotal,fill = ssp)) +
   scale_fill_manual(values = c("#FF1493", "#CCEDB1", "#41B7C4"))+
   scale_y_continuous(expand=c(0,0),limits =  c(0,.1),breaks = c(0,0.025,0.050,0.075,0.1),labels = c("0","0.25%","0.5%","0.75%","1.0%"))+
   scale_x_continuous(limits =  c(0.5,10.5),breaks = seq(from=1, to=10, by=1))
+
